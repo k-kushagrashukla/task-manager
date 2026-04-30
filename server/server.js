c997eb05c839e08d27f80dbd316e6391afbb679e
k-kushagrashukla <kushagra.shukla396@gmail.com>
2026-04-30 20:00:37 +0530

    final server deploy fix

--- server/server.js
+++ server/server.js
@@ -7,20 +7,23 @@ import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use("/api/auth", authRoutes)
-app.use("/api/projects", projectRoutes)
-app.use("/api/tasks", taskRoutes)
+app.use(cors({
+  origin: "*"
+}));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB Connected"))
-  .catch(err => console.log(err))
+  .catch(err => console.log("DB Error:", err));
 
-app.listen(5000, () => console.log("Server running on 5000"));
-
-console.log("MONGO URI:", process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
