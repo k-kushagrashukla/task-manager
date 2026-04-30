a786d7510fb182430eea9e76833aa9449ccc2275
k-kushagrashukla <kushagra.shukla396@gmail.com>
2026-04-30 20:16:21 +0530

    adding /api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-z19s.onrender.com"
+  baseURL: "https://task-manager-z19s.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
